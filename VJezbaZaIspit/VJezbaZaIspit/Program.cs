bd14098b0a01daf7c398b9d5e11b73588bd6e2ab
LeonCicak <leoncicak07@gmail.com>
2024-12-09 13:28:58 +0100

    Add files via upload

---
+++ VJezbaZaIspit/VJezbaZaIspit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VJezbaZaIspit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public class KlasaX
+        {
+            private int broj = 10;
+
+            public int Broj
+            {
+                get { return broj; }
+                set { broj = value; }
+            }
+        }
+
+        // Korištenje klase
+        KlasaX instanca = new KlasaX();
+        Console.WriteLine(instanca.Broj);
+        }
+    }
+}
